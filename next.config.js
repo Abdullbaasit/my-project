3cd4c86b015678bad8d9872e7712284c4b92e499
Adepoju Taiwo <fimihanadepoju@gmai.com>
2026-04-26 07:54:24 +0100

    updating to config changes

--- next.config.js
+++ next.config.js
@@ -1,8 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['cdn.dummyjson.com'],
-  },
+  remotePatterns: [
+    {
+      protocol: 'https',
+      hostname: 'dummyjson.com',
+      pathname: '/**',
+    },
+    {
+      protocol: 'https',
+      hostname: 'i.dummyjson.com',
+      pathname: '/**',
+    },
+  ],
+},
 }
 
 export default nextConfig
